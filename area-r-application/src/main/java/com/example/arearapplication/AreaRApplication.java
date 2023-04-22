523b02e1df519274685878fe123292fab58d1783
KubaBart <256466@student.pwr.edu.pl>
2023-04-22 16:55:46 +0200

    project init

---
+++ area-r-application/src/main/java/com/example/arearapplication/AreaRApplication.java
@@ -0,0 +1,13 @@
+package com.example.arearapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreaRApplication.class, args);
+    }
+
+}
---
+++ area-r-application/src/test/java/com/example/arearapplication/AreaRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arearapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreaRApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
